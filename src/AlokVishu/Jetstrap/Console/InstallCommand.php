a8eef3999ff0ee52a405742a735c8da88455a40d
Aloksoni <alok@pixinvent.com>
2021-09-03 12:36:04 +0530

    comment updated

--- src/AlokVishu/Jetstrap/Console/InstallCommand.php
+++ src/AlokVishu/Jetstrap/Console/InstallCommand.php
@@ -137,7 +137,7 @@ class InstallCommand extends Command
 
         $this->line('');
         $this->info('Bootstrap scaffolding swapped for livewire successfully.');
-        $this->comment('Please execute the "npm install && npm run dev" command to build your assets.');
+        $this->comment('Please execute the "npm install && npm run mix" OR "yarn && yarn mix" command to build your assets.');
     }
 
     /**
